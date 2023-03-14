cf09c1b4821c69343c56647f6b5a9b068d4b3b37
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-03-14 13:09:03 -0300

    chore: initialized Spring project

---
+++ src/main/java/io/lucasprojects/granna/GrannaApplication.java
@@ -0,0 +1,13 @@
+package io.lucasprojects.granna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrannaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrannaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/lucasprojects/granna/GrannaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lucasprojects.granna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrannaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
