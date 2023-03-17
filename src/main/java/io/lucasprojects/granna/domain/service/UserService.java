b708c7d58cf3cfffeb80e5f042f5d8b2b8151e79
Lucas Ferreira <ferreiralucas090@gmail.com>
2023-03-17 17:34:57 -0300

    feat: crud user interface - inital implementation

--- src/main/java/io/lucasprojects/granna/domain/service/UserService.java
+++ src/main/java/io/lucasprojects/granna/domain/service/UserService.java
@@ -1,40 +1,72 @@
 package io.lucasprojects.granna.domain.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.lucasprojects.granna.domain.exception.ResourceBadRequestException;
+import io.lucasprojects.granna.domain.exception.ResourceNotFoundException;
+import io.lucasprojects.granna.domain.model.User;
+import io.lucasprojects.granna.domain.repository.UserRepository;
 import io.lucasprojects.granna.dto.User.UserRequestDTO;
 import io.lucasprojects.granna.dto.User.UserResponseDTO;
 
 public class UserService implements ICRUDService<UserRequestDTO, UserResponseDTO> {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public List<UserResponseDTO> getAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                    .map(user -> mapper.map(users, UserResponseDTO.class))
+                    .collect(Collectors.toList());
     }
 
     @Override
     public UserResponseDTO getById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+        Optional<User> user = userRepository.findById(id);
+
+        if(user.isEmpty())
+            throw new ResourceNotFoundException("Unable to find user with id: " + id);
+
+        return mapper.map(user.get(), UserResponseDTO.class);
     }
 
     @Override
     public UserResponseDTO add(UserRequestDTO dto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+
+        if(dto.getEmail() == null || dto.getPassword() == null)
+            throw new ResourceBadRequestException("Email or password is null.");
+
+        User user = mapper.map(dto, User.class);
+        user = userRepository.save(user);
+
+        return mapper.map(user, UserResponseDTO.class);
     }
 
     @Override
     public UserResponseDTO update(Long id, UserRequestDTO dto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        getById(id);
+
+        User user = mapper.map(dto, User.class);
+
+        user.setId(id);
+        user = userRepository.save(user);
+        return mapper.map(user, UserResponseDTO.class);
     }
 
     @Override
     public void delete(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        getById(id);
+        userRepository.deleteById(id);
     }
     
 }
